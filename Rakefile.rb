23ee9f3299304b0dd63cb849ba7bac93d330c2c2
Christian Köstlin <christian.koestlin@esrlabs.com>
2012-07-12 13:39:26 +0200

    added push_gems task
    Change-Id: Id6cabfc2b5c9defbfd7dba1815c2f0f3a6f1099f

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,4 +1,4 @@
-projects = ['frazzle', 'cxxproject', 'cxxproject_gcctoolchain', 'cxxproject_clangtoolchain', 'rubydsl', 'cxxproject_console']
+projects = ['frazzle', 'cxxproject', 'cxxproject_gcctoolchain', 'cxxproject_clangtoolchain', 'rubydsl', 'cxxproject_console', 'cxxproject_valgrind']
 desc 'build all projects'
 task :build do
   projects.each do |p|
@@ -11,6 +11,18 @@ end
 desc 'install all built gems'
 task :install_gems => :build do
   sh "gem install #{FileList[projects.map{|p|"../#{p}/pkg/*.gem"}].join(' ')}"
+end
+
+desc 'pushes all gems to rubygems'
+task :push_gems => :build do
+  FileList[projects.map{|p|"../#{p}/pkg/*.gem"}].each do |gem|
+    begin
+      sh "gem push #{gem}"
+    rescue Exception => e
+      puts "problems with pushing gem #{gem}:"
+      puts e
+    end
+  end
 end
 
 task :default => :install_gems
