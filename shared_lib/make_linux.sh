9223185812077e184edb6d1dda769bb7a59316ec
Christian Köstlin <christian.koestlin@esrlabs.com>
2013-01-12 19:45:05 +0100

    added linux makefile and some links
    Change-Id: I50cbdf01151faea00bc2bfa80309135ebcdf295d

--- shared_lib/make_linux.sh
+++ shared_lib/make_linux.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -v
+# seems to be the reference: http://tldp.org/HOWTO/Program-Library-HOWTO/shared-libraries.html
+# it would be probably wise to add some versioning flags to dynamic_lib
 mkdir -p out/libs
 g++ -c -fPIC lib1.cpp -o out/lib1.o
-g++ -shared -o out/libs/liblib1.so out/lib1.o
+g++ -shared -Wl,-soname,liblib1.so -o out/libs/liblib1.so out/lib1.o
 g++ -c main.cpp -o out/main.o
 g++ -o out/main.exe out/main.o -Lout/libs -llib1
